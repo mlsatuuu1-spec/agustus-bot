59ff7fceac6771bc4cce0f4d3ef00d84ebf6abad
mlsatuuu1-spec <mlsatuuu1@gmail.com>
2026-08-01 23:33:58 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -11,10 +11,10 @@ function random(min, max) {
 }
 
 // ======================
-// FORMAT
+// FORMAT ANGKA
 // ======================
 
-function formatNumber(num) {
+function format(num) {
 
     return new Intl.NumberFormat("id-ID").format(num);
 
@@ -24,54 +24,30 @@ function formatNumber(num) {
 // TIER
 // ======================
 
-function getTier(point) {
+function getTier(points) {
 
-    if (point >= 2000)
-        return {
-            emoji: "🔥",
-            name: "Cashback Overlord"
-        };
+    if (points >= 2000)
+        return "🔥 Cashback Overlord";
 
-    if (point >= 1200)
-        return {
-            emoji: "👑",
-            name: "Cashback Sultan"
-        };
+    if (points >= 1200)
+        return "👑 Cashback Sultan";
 
-    if (point >= 750)
-        return {
-            emoji: "💎",
-            name: "Cashback Elite"
-        };
+    if (points >= 750)
+        return "💎 Cashback Elite";
 
-    if (point >= 400)
-        return {
-            emoji: "🏦",
-            name: "Cashback Investor"
-        };
+    if (points >= 400)
+        return "🏦 Cashback Investor";
 
-    if (point >= 200)
-        return {
-            emoji: "💰",
-            name: "Cashback Grinder"
-        };
+    if (points >= 200)
+        return "💰 Cashback Grinder";
 
-    if (point >= 75)
-        return {
-            emoji: "🪙",
-            name: "Cashback Hunter"
-        };
+    if (points >= 75)
+        return "🪙 Cashback Hunter";
 
-    if (point >= 25)
-        return {
-            emoji: "💵",
-            name: "Cashback Rookie"
-        };
+    if (points >= 25)
+        return "💵 Cashback Rookie";
 
-    return {
-        emoji: "▫️",
-        name: "-"
-    };
+    return "▫️ Belum ada Tier";
 
 }
 
@@ -91,18 +67,18 @@ text:"🇮🇩 Kamu berhasil menangkap Garuda!"
 
 {
 emoji:"🎖️",
-item:"Medali",
+item:"Medali Kemerdekaan",
 point:4,
 chance:10,
-text:"🎖️ Garuda menghadiahimu sebuah medali."
+text:"🎖️ Garuda menghadiahimu medali."
 },
 
 {
 emoji:"🚗",
 item:"Avanza",
 point:4,
-chance:7,
-text:"🚗 Garuda menjatuhkan sebuah Avanza."
+chance:5,
+text:"🚗 Entah dari mana Garuda menjatuhkan Avanza."
 },
 
 {
@@ -125,7 +101,7 @@ text:"🥭 Garuda menjatuhkan mangga."
 emoji:"🩴",
 item:"Sandal Jepit",
 point:2,
-chance:10,
+chance:8,
 text:"🩴 Lumayan dapat sandal."
 },
 
@@ -134,7 +110,7 @@ emoji:"💩",
 item:"Tai Ayam",
 point:-5,
 chance:18,
-text:"💩 Garuda malah menjatuhkan tai ayam."
+text:"💩 Astaga... Garuda malah menjatuhkan tai ayam."
 },
 
 {
@@ -149,7 +125,7 @@ text:"🐄 Kamu kena tai kebo."
 emoji:"🦟",
 item:"Nyamuk",
 point:-2,
-chance:5,
+chance:6,
 text:"🦟 Digigit nyamuk."
 },
 
@@ -157,7 +133,7 @@ text:"🦟 Digigit nyamuk."
 emoji:"🪨",
 item:"Kesandung Batu",
 point:-1,
-chance:1.5,
+chance:4,
 text:"🪨 Kamu kesandung batu."
 },
 
@@ -165,7 +141,7 @@ text:"🪨 Kamu kesandung batu."
 emoji:"👑",
 item:"Mahkota Garuda",
 point:20,
-chance:0.25,
+chance:0.5,
 text:"👑 JACKPOT!! Mahkota Garuda!"
 },
 
@@ -173,21 +149,23 @@ text:"👑 JACKPOT!! Mahkota Garuda!"
 emoji:"💰",
 item:"Karung Robux",
 point:15,
-chance:0.25,
+chance:0.5,
 text:"💰 JACKPOT!! Karung Robux!"
 }
 
 ];
 
 // ======================
-// RANDOM REWARD
+// RANDOM HADIAH
 // ======================
 
 function randomReward() {
 
-    let total = 0;
-
-    rewards.forEach(r => total += r.chance);
+    const total =
+        rewards.reduce(
+            (sum, reward) => sum + reward.chance,
+            0
+        );
 
     let roll = Math.random() * total;
 
@@ -205,15 +183,41 @@ function randomReward() {
 }
 
 // ======================
+// MONUMEN
+// ======================
+
+const monuments = [
+
+"Monas",
+
+"Borobudur",
+
+"Prambanan",
+
+"Suramadu",
+
+"Jam Gadang",
+
+"GWK",
+
+"Tugu Pahlawan",
+
+"Istana Merdeka"
+
+];
+
+// ======================
 
 module.exports = {
 
 random,
 
-formatNumber,
+format,
 
 getTier,
 
-randomReward
+randomReward,
+
+monuments
 
 };
