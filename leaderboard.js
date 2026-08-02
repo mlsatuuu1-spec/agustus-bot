ae23d5b5aad5a37a95ff3a3c422bacb8fed1f232
mlsatuuu1-spec <mlsatuuu1@gmail.com>
2026-08-02 09:29:15 +0700

    Create leaderboard.js

---
+++ leaderboard.js
@@ -0,0 +1,100 @@
+const { EmbedBuilder } = require("discord.js");
+const database = require("./database");
+
+class Leaderboard {
+
+    constructor(client) {
+        this.client = client;
+    }
+
+    async update() {
+
+        if (!process.env.LEADERBOARD_CHANNEL) return;
+
+        try {
+
+            const channel = await this.client.channels.fetch(
+                process.env.LEADERBOARD_CHANNEL
+            );
+
+            const users = database
+                .leaderboard()
+                .slice(0, 10);
+
+            let text = "";
+
+            if (users.length === 0) {
+
+                text = "Belum ada pemain.";
+
+            } else {
+
+                users.forEach((user, index) => {
+
+                    let medal = "🏅";
+
+                    if (index === 0) medal = "🥇";
+                    else if (index === 1) medal = "🥈";
+                    else if (index === 2) medal = "🥉";
+
+                    text += `${medal} **${user.username}**
+⭐ ${user.points} poin
+🦅 Garuda : ${user.garuda || 0}
+
+`;
+
+                });
+
+            }
+
+            const embed = new EmbedBuilder()
+
+                .setColor("#FFD700")
+
+                .setTitle("🏆 LEADERBOARD GARUDA")
+
+                .setDescription(text)
+
+                .setFooter({
+                    text: "Update Otomatis"
+                })
+
+                .setTimestamp();
+
+            if (database.events.leaderboardMessage) {
+
+                try {
+
+                    const msg = await channel.messages.fetch(
+                        database.events.leaderboardMessage
+                    );
+
+                    await msg.edit({
+                        embeds: [embed]
+                    });
+
+                    return;
+
+                } catch (err) {}
+
+            }
+
+            const msg = await channel.send({
+                embeds: [embed]
+            });
+
+            database.events.leaderboardMessage = msg.id;
+
+            database.saveEvents();
+
+        } catch (err) {
+
+            console.error(err);
+
+        }
+
+    }
+
+}
+
+module.exports = Leaderboard;
